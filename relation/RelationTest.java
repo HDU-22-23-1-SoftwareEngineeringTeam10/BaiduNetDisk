41796600cb0d2a1c8339977d8109cdf0628d86d4
NOMAD-HT <91360077+NOMAD-HT@users.noreply.github.com>
2022-12-05 11:09:28 +0800

    关系系统

---
+++ relation/RelationTest.java
@@ -0,0 +1,10 @@
+package com.baidu.relation;
+
+public class RelationTest {
+
+	public static void main(String[] args) {
+		TopRelation r1 = new TopRelation();
+		r1.relation();
+	}
+
+}
---
+++ relation/Relationship.java
@@ -0,0 +1,5 @@
+package com.baidu.relation;
+
+public interface Relationship {
+	void add();
+}
