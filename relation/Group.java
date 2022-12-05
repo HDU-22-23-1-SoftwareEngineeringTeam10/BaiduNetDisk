cb268042fb05fc593077310300c8cc796d7fdaa9
NOMAD <1908182668@qq.com>
2022-12-05 12:25:59 +0800

    Relation关系系统

---
+++ relation/Group.java
@@ -0,0 +1,36 @@
+package com.baidu.relation;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Group implements Relationship,DataBaseAware<Integer, Group> {
+    private List<User> userList;
+    private Integer groupId;
+
+    private static AtomicInteger autoIncrement = new AtomicInteger(0);
+
+    public Group() {
+        this.groupId = autoIncrement.incrementAndGet();
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+    
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    @Override
+    public Integer getKey() {
+        return groupId;
+    }
+    
+    public void add() {
+    	System.out.println("添加群聊");
+    }
+}
